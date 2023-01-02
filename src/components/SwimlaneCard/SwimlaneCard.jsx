a01831bf6d49c32b54aa48d5f1112ffe8ee0a2cf
sijincodes <sijinmathews03@gmail.com>
2023-01-02 21:00:10 +0100

    drag and drop partially done

--- src/components/SwimlaneCard/SwimlaneCard.jsx
+++ src/components/SwimlaneCard/SwimlaneCard.jsx
@@ -2,12 +2,16 @@ import React from "react";
 
 import "./SwimlaneCard.css";
 
-function SwimlaneCard() {
+function SwimlaneCard({testData}) {
+  const dragStarted = (e,id) =>{
+    console.log("drag started");
+    e.dataTransfer.setData('id',id)
+  }
   return (
     <>
-      <div className="card">
-        <h1 className="heading">Job Title: dev jr</h1>
-        <p className="desc">Company: Flink</p>
+      <div className="card" draggable onDragStart={ e => {dragStarted(e,testData.id)}}>
+        <h1 className="heading">Job Title: {testData.title}</h1>
+        <p className="desc">Company: {testData.company}</p>
         <p>Tag</p>
         <button className="button">See More</button>
       </div>
