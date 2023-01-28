6c5745dbddb96a5e712efd1e83af100eac26c593
sijincodes <sijinmathews03@gmail.com>
2023-01-28 15:50:59 +0100

    search

--- src/components/SearchInput/SearchInput.jsx
+++ src/components/SearchInput/SearchInput.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React from "react";
 
-import "./SearchInput.css"
+import "./SearchInput.css";
 
 function Search({ setSearchQuery, searchQuery }) {
   return (
     <>
-        
-        <input type="text" className="searchBoxInputBox"  value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-            placeholder="Search for applied Job Role/Company" />
+      <input
+        type="text"
+        className="searchBoxInputBox"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        placeholder="Search for applied Job Role/Company"
+      />
     </>
-  )
+  );
 }
 
-export default Search
+export default Search;
