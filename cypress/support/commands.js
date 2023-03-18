730de349b0cac1a13309207475329a3ba178e785
sijincodes <sijinmathews03@gmail.com>
2023-03-18 11:22:45 +0100

    e2e fix for login timeout

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,22 +1,26 @@
 import {
-    LOGIN_URL,
-    EMAIL_INPUT,
-    PASSWORD_INPUT,
-    LOGIN_BUTTON,
-  } from "../e2e/pages/Login";
+  LOGIN_URL,
+  EMAIL_INPUT,
+  PASSWORD_INPUT,
+  LOGIN_BUTTON,
+} from "../e2e/pages/Login";
 
+Cypress.Commands.add("login", () => {
+  cy.intercept("GET", JOB_API).as("fetchJobs");
+  cy.visit(LOGIN_URL);
+  cy.fixture("user").then((user) => {
+    const email = user.email;
+    const password = user.password;
 
-Cypress.Commands.add('login', () => { 
-    cy.visit(LOGIN_URL);
-    cy.fixture("user").then((user) => {
-      const email = user.email;
-      const password = user.password;
+    cy.get(EMAIL_INPUT).type(email);
+    cy.get(PASSWORD_INPUT).type(password);
+    cy.get(LOGIN_BUTTON).click();
 
-      cy.get(EMAIL_INPUT).type(email);
-      cy.get(PASSWORD_INPUT).type(password);
-      cy.get(LOGIN_BUTTON).click();
+    cy.wait("@fetchJobs").then(({ response }) => {
+      expect(response.statusCode).to.eq(201);
     });
- })
+  });
+});
 //
 //
 // -- This is a child command --
